95493aa25b3eb67be4eb46af88672602ed0b113b
lingjie <979520102@qq.com>
2019-11-04 09:25:07 +0800

    Initial Commit

---
+++ DailyPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyPractice
+//
+//  Created by Kede on 2019/11/4.
+//  Copyright © 2019 Clare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
